9f82b3f80cc9d29b5e043f6b240f95640dae67a0
Shehab Badawy <69417326+shehab-badawy@users.noreply.github.com>
2021-12-16 19:50:19 +0200

    developing pass2 assembler building object code for format 1 and format 4

---
+++ src/com/company/Label.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Label {
+    String labelName;
+    int loctr;
+
+    public Label(String labelName, int loctr) {
+        this.labelName = labelName;
+        this.loctr = loctr;
+    }
+}
